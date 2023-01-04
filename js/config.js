46dbeae75ebf121db36e6a15fdcc35b948f42f91
小黎 <[22708206@qq.com]>
2023-01-04 17:58:58 +0800

    chore(all): 暂存
    暂存

---
+++ js/config.js
@@ -0,0 +1,13 @@
+/**
+ * @file config.js 配置项
+ * @description 提炼网站基础的配置，可改动最小后定制自己的网站
+ * @author xiao li
+ * @copyright 黎<https://www.xiaoli.vip>
+ * @createDate 2023-01-04 12:26
+ */
+
+const loading = false
+
+window.myConfig = {
+
+}
